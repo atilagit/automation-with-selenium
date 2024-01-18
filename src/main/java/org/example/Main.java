b494309decef1e06077b767328f8142ca38c965f
Atila Silva <atila__7@hotmail.com>
2024-01-17 23:36:46 -0300

    Change Hello World

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        WebDriver driver = WebDriverManager.chromedriver().create();
+        driver.get("https://www.google.com");
+        driver.quit();
     }
 }
